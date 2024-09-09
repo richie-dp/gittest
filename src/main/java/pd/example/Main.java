6d57a86efa54d5f4e9971d2def7c2de97ee0c449
p.d <pan.du@tongdun.net>
2024-09-09 11:11:56 +0800

    init

---
+++ src/main/java/pd/example/Main.java
@@ -0,0 +1,12 @@
+package pd.example;
+
+/**
+ * @Author pan.du
+ * @Date 2024/9/9 11:04
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
